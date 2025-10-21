4be375b003dc7740fe5afdb3278a56a37fb986c6
jmkim <isugar150@gmail.com>
2025-10-21 16:54:07 +0900

    ⚡️

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,32 @@
-﻿import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react(), dts({ entryRoot: 'src', include: ['src'], insertTypesEntry: true, tsconfigPath: './tsconfig.app.json', outDir: 'dist', rollupTypes: true })],
+  plugins: [
+    react(),
+    dts({
+      entryRoot: "src",
+      include: ["src"],
+      insertTypesEntry: true,
+      tsconfigPath: "./tsconfig.app.json",
+      outDir: "dist",
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
-      entry: 'src/index.ts',
-      name: 'ReactWeekline',
-      formats: ['es', 'cjs'],
-      fileName: (format) => (format === 'es' ? 'index.js' : 'index.cjs'),
+      entry: "src/index.ts",
+      name: "ReactWeekline",
+      formats: ["es", "cjs"],
+      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
